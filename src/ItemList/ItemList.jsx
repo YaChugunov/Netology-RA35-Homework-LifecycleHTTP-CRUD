b571aea7523fa305c7928bc17513c182e2d0b7bc
YaChugunov <informed.looser@gmail.com>
2022-05-17 16:03:24 +0300

    2 files modified

--- src/ItemList/ItemList.jsx
+++ src/ItemList/ItemList.jsx
@@ -4,7 +4,7 @@ export default function ItemList({ notes, handleDelete }) {
   return notes.map((note) => (
     <div className="note-container" key={note.id}>
       <button className="note-close" onClick={() => handleDelete(note.id)}>
-        <img src="close.png" alt="close" className="imgClose" />
+        X
       </button>
       <div className="content">{note.content}</div>
     </div>
